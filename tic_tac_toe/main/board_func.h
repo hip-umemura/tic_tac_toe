c663529c7cd76d7c74250433d605fddf0b361800
h_iwata <h_iwata@hip-pro.info>
2024-05-16 16:13:36 +0900

    board_func.c 編集１回目

--- tic_tac_toe/main/board_func.h
+++ tic_tac_toe/main/board_func.h
@@ -1,1 +1,24 @@
 #pragma once
+
+typedef enum
+{
+  BLANK,
+  PLAYER1,
+  PLAYER2
+} MARK;
+
+typedef enum
+{
+  NONE,
+  PLAYER1_WIN,
+  PLAYER2_WIN,
+  DRAW
+} RESULT;
+
+//関数のプロトタイプ宣言
+int ScanInput(void);
+void PrintBoard(MARK*);
+RESULT CheckBoard(MARK*);
+
+#define TRUE 1
+#define FALSE 0
