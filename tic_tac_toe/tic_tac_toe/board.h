f969f0ac30ce51e71546505922211847b1650947
田坂共平 <k_tasaka@hip-pro.info>
2024-05-20 14:02:30 +0900

    board.h変更

--- tic_tac_toe/tic_tac_toe/board.h
+++ tic_tac_toe/tic_tac_toe/board.h
@@ -1,1 +1,13 @@
-#pragma once
+#define BOARD_SIZE (3)
+typedef enum
+{
+	WIN,
+	DRAW,
+	NONE,
+}RESULT;
+
+//  プロトタイプ宣言
+void print_now_board(void); 
+int put_piece(int row, int column PLAYER player);
+RESULT judge_game(void);
+void clean_board(void);
