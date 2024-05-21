5f1a74b24ee909cfd3201e27553468f100429956
r_morimoto <r_morimoto@TRAIN24-07>
2024-05-21 12:00:49 +0900

    game_progressを完了しました。

--- tic_tac_toe/tic_tac_toe/basic.h
+++ tic_tac_toe/tic_tac_toe/basic.h
@@ -4,8 +4,8 @@
 #include <stdio.h>
 
 #define NAME_LEN (10)	//ƒvƒŒƒCƒ„[–¼‚Ì’·‚³ãŒÀ
-#define FALSE (0)	//gFALSEh‚ğ0‚Æ‚·‚é
-#define TRUE (1)	 //gTRUEh‚ğ1‚Æ‚·‚é
+#define FALSE (0)		//gFALSEh‚ğ0‚Æ‚·‚é
+#define TRUE (1)		//gTRUEh‚ğ1‚Æ‚·‚é
 
 typedef enum
 {
@@ -13,11 +13,7 @@ typedef enum
 	SECOND_TURN,
 } TURN;
 
-<<<<<<< HEAD
 typedef struct player
-=======
-typedef struct playaer
->>>>>>> 2c948bdb7dc2d619b8721acbb517ef3abee730d8
 {
 	char* name; //player–¼
 	char piece; //‹î
