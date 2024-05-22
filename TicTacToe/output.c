cfbd159509d399dbb2cd0cece6aeea2cd169b4e2
i_shimeno <i_shimeno@tr.hip-pro.net>
2024-05-22 11:00:44 +0900

    Output_Info 関数の完了

--- TicTacToe/output.c
+++ TicTacToe/output.c
@@ -8,15 +8,20 @@
 //char board_info	ボードの情報を持つ変数
 void Output_Info(int tern_info, char board_info [3][3])
 {
-
+	if (tern_info % 2 == 0) {
+		printf("○のターンです。\n");
+	}
+	else {
+		printf("×のターンです。\n");
+	}
 	printf("-----------------\n");
 	for (int column = 2; column >= 0; column--) {
 		for (int row = 0; row <= 2; row++) {
 			printf("  %c  ", board_info[column][row]);
 			printf("|");
-
 		}
-		//バックスペースをし、半角スペースで上書き
+
+		//バックスペースを入力し、半角スペースで上書き
 		printf("\b \n");
 		printf("-----------------\n");
 	}
