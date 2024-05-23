f34d620f0132322bf00da6a1095ea6b0955d2156
k_kumamoto <k_kumamoto@tr.hip-pro.net>
2024-05-23 15:45:58 +0900

    名前入力の修正

--- tic_tac_toe/tic_tac_toe/board.c
+++ tic_tac_toe/tic_tac_toe/board.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include "board.h"
 
-char board[BOARD_SIZE][BOARD_SIZE];
+char board[BOARD_SIZE][BOARD_SIZE];	// 盤面をグローバル変数で定義（char型）
 
 void print_now_board(void) { // 盤面の状況をを表示
-	int column; 
+
+	int column;
+
 	printf("　 1   2   3\n");
 	printf(" -------------\n");
 	printf("1|");
@@ -27,8 +29,8 @@ void print_now_board(void) { // 盤面の状況をを表示
 
 int put_piece(int row, int column, PLAYER player) {
 	row--; // 1から始まる入力を0から始まるインデックスに変換
-	column--; // 同上
-	if (row >= 0 && row <= BOARD_SIZE && column >= 0 && column <= BOARD_SIZE && board[row][column] == ' ') {
+	column--;
+	if (((row >= 0) && (row < BOARD_SIZE)) && ((column >= 0) && (column < BOARD_SIZE)) && (board[row][column] == ' ')) {
 		board[row][column] = player.piece;
 		return TRUE; //駒を配置出来た場合にはTRUE(1)
 	}
@@ -39,32 +41,32 @@ int put_piece(int row, int column, PLAYER player) {
 
 RESULT judge_game(void) {
 	for (int row = 0; row < BOARD_SIZE; row++) {
-		if (board[row][0] == board[row][1] && board[row][1] == board[row][2] && board[row][0] != ' ') {
+		if ((board[row][0] == board[row][1]) && (board[row][1] == board[row][2]) && (board[row][0] != ' ')) {
 			return WIN; //　行をチェックしてOXがそろっているとき
 		}
 	}
 	for (int column = 0; column < BOARD_SIZE; column++) {
-		if (board[0][column] == board[1][column] && board[1][column] == board[2][column] && board[0][column] != ' ') {
+		if ((board[0][column] == board[1][column]) && (board[1][column] == board[2][column]) && (board[0][column] != ' ')) {
 			return WIN; //　列をチェックしてOXがそろっているとき
 		}
 	}
-	if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') {
+	if ((board[0][0] == board[1][1]) && (board[1][1] == board[2][2]) && (board[0][0] != ' ')) {
 		return WIN; //　右斜めがそろっているとき
 	}
-	else if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ') {
+	else if ((board[0][2] == board[1][1]) && (board[1][1] == board[2][0]) && (board[0][2] != ' ')) {
 		return WIN; // 左斜めがそろっているとき
 	}
 	for (int row = 0; row < BOARD_SIZE; row++) {
 		for (int column = 0; column < BOARD_SIZE; column++) {
 			if (board[row][column] == ' ') {
-				return NONE; //まだ空きスペースがあるならNONE
+				return NONE; // まだ空きスペースがあるならNONE
 			}
 		}
 	}
-	return DRAW; //全てのスペースが埋まっているならDRAW
+	return DRAW; // 全てのスペースが埋まっているならDRAW
 }
 
-void clean_board(void) { //盤面を初期化
+void clean_board(void) { // 盤面を初期化
 	for (int row = 0; row < BOARD_SIZE; row++) {
 		for (int column = 0; column < BOARD_SIZE; column++) {
 			board[row][column] = ' ';
--- tic_tac_toe/tic_tac_toe/main.c
+++ tic_tac_toe/tic_tac_toe/main.c
@@ -1,4 +1,5 @@
-﻿#include <stdio.h>
+﻿// ゲーム（三目並べ）全体を管理するファイル
+#include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <wchar.h>
@@ -8,18 +9,21 @@
 #include "tutorial.h"
 #include "board.h"
 
+// 現在のプレイヤーの交代を行う
 TURN change_turn(TURN now) {
 	printf("先手と後手のターンを交代します\n");
+
 	if (now == FIRST_TURN) {
-		now = SECOND_TURN;
+		now = SECOND_TURN;	// 先手→後手
 		return now;
 	}
 	else {
-		now = FIRST_TURN;
+		now = FIRST_TURN;	// 後手→先手
 		return now;
 	}
 }
 
+// 勝敗の表示を行う。
 void print_result(RESULT result, PLAYER player) {
 	if (result == DRAW) {
 		printf("引き分け！\n");
@@ -29,10 +33,14 @@ void print_result(RESULT result, PLAYER player) {
 	}
 }
 
+// ゲーム終了後、再度ゲームをプレイするか否か
 int retry_game(void) {
-	char userInput;
+
+	char userInput;	// プレイヤーが入力した値
+
 	printf("再プレイしますか(YES...1 / No...1以外):");
 	scanf_s("%c", &userInput, 1);
+
 	if (userInput == '1') {
 		return TRUE;
 	}
@@ -41,12 +49,12 @@ int retry_game(void) {
 	}
 }
 
-// 三目並べ全体の処理
+// チュートリアルとプレイヤー名の入力終了後、ゲーム全体の進行を行う。
 void game_progress(char* name1, char* name2)
 {
-	PLAYER game_player1;				// 先手プレイヤー
-	PLAYER game_player2;				// 後手プレイヤー
-	PLAYER now_game_player;				// 現在のプレイヤー
+	PLAYER game_player1;		// 先手のプレイヤー情報
+	PLAYER game_player2;		// 後手のプレイヤー情報
+	PLAYER now_game_player;		// 現在のプレイヤー情報
 
 	game_player1.name = name1;
 	game_player1.piece = 'O';
@@ -54,25 +62,26 @@ void game_progress(char* name1, char* name2)
 	game_player2.name = name2;
 	game_player2.piece = 'X';
 
-	TURN player_game_turn = FIRST_TURN;					// プレイヤーのターン（初期値）
+	TURN player_game_turn = FIRST_TURN;	// プレイヤーのターン（初期値）
 
-	RESULT game_result = NONE;							// ゲームの結果を格納する
+	RESULT game_result = NONE;			// ゲームの結果（初期値）
 
-	int  game_horizontal_axis;						// 盤面の横軸（int型）
-	int  game_vertical_axis;						// 盤面の縦軸（int型）
+	int  game_horizontal_axis;								// 盤面の横軸（int型）
+	int  game_vertical_axis;								// 盤面の縦軸（int型）
 	char len_game_horizontal_axis[LEN_HORIZONTAL_AXIS + 1];	// 盤面の横軸（char型）
 	char len_game_vertical_axis[LEN_VERTICAL_AXIS + 1];		// 盤面の縦軸（char型）
 
-	int error_count;
+	int error_count;	// 不正な入力した場合にエラー文を出させる条件式用変数
 
 	clean_board();		// 盤面を初期化
 
 	print_now_board();	// 盤面を表示
 
+	// ゲームの勝敗が決するまでループする。
 	while (game_result == NONE) {
 		game_horizontal_axis = 0;
-		game_vertical_axis = 0;
-		error_count = 0;	// 不正な入力した場合にエラー文を出させる条件変数
+		game_vertical_axis   = 0;
+		error_count          = 0;
 
 		// 現在のプレイヤーを代入
 		if (player_game_turn == FIRST_TURN) {
@@ -89,10 +98,12 @@ void game_progress(char* name1, char* name2)
 			}
 
 			printf("%sさん、駒を置く座標を入力してください：", now_game_player.name);
-			scanf_s("%s ", len_game_horizontal_axis, LEN_HORIZONTAL_AXIS + 1);
+			scanf_s("%s", len_game_horizontal_axis, LEN_HORIZONTAL_AXIS + 1);
 			scanf_s("%s", len_game_vertical_axis, LEN_VERTICAL_AXIS + 1);
+			while (getchar() != '\n');	// バッファをクリアにしている
 			error_count++;
 
+			// 入力された値が数字か数字以外か判断
 			if ((isdigit(len_game_horizontal_axis[LEN_HORIZONTAL_AXIS - 1]) != 0) &&
 				(isdigit(len_game_vertical_axis[LEN_VERTICAL_AXIS - 1]) != 0)) {
 				game_horizontal_axis = atoi(len_game_horizontal_axis);
@@ -102,42 +113,43 @@ void game_progress(char* name1, char* name2)
 		} while (((game_horizontal_axis < 1) || (game_horizontal_axis > 3)) ||
 			((game_vertical_axis < 1) || (game_vertical_axis > 3)));
 
+		// 入力された盤面の座標に駒を配置し、プレイヤーの交代を行う
 		if (put_piece(game_horizontal_axis, game_vertical_axis, now_game_player) == TRUE) {
 
-			print_now_board();									// 駒配置後、再度盤面を表示
+			print_now_board();				// 駒配置後、再度盤面を表示
 
 			printf("駒を配置しました。\n");
 
-			game_result = judge_game();							// ゲームの結果を代入
+			game_result = judge_game();		// ゲームの結果を代入
 
 			if (game_result == NONE) {
-				player_game_turn = change_turn(player_game_turn);//　現在のプレイヤー情報を入れ替える
+				player_game_turn = change_turn(player_game_turn);	// 現在のプレイヤーを交代する
 			}
-		}
-		else {
+		} else {
 			printf("\x1b[31m不正な入力です。再度入力してください！\x1b[39m\n");
 		}
 	}
-	print_result(game_result, now_game_player);					// ゲームの結果を表示する
+	print_result(game_result, now_game_player);	// ゲームの結果を表示する
 }
 
 int main(void) {
-	int start_tutorial;
+	int start_tutorial;		// チュートリアルを表示する条件変数
 	char name[PLAYERS][NAME_LEN];
-	int name_array;
+	int name_array;			// プレイヤー名の要素数
 	int player_count = 0;
 
 	printf("チュートリアルを見ますか(YES...1 / NO...1以外)：");
 	scanf_s("%d", &start_tutorial);
-	while (getchar() != '\n')
-	  ;
+	while (getchar() != '\n');
 
+	// チュートリアルを表示する
 	if (start_tutorial == TRUE) {
 		view_tutorial();
 	}
 
 	printf("ゲーム開始！\n");
 
+	// 先手のプレイヤー名を入力
 	do {
 	  do {
 		name_array = 0;
@@ -160,12 +172,13 @@ int main(void) {
 		} while (name[player_count][name_array] != '\0');
 
 		while (getchar() != '\n')
-		  ;
+			;
 
 	  } while ((name[player_count][name_array] != '\0') || (name_array == 0));
 	  player_count++;
 	} while (player_count != PLAYERS);
 
+	// プレイヤー名入力後、ゲームを行う
 	do
 	{
 		game_progress(name[0], name[1]);
