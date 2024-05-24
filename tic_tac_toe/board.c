b3c3da41e7d39610060a3ea24401325c5badf169
t_fujii <t_fujii@hip-pro.info>
2024-05-24 15:30:12 +0900

    commit

---
+++ tic_tac_toe/board.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<locale.h>
+#include "board.h"
+
+
+void ShowBoard(square[9]) {
+	setlocale(LC_ALL, "ja_JP.UTF-8");
+	printf(" -------------------- \n|      |      |      |\n");
+	for (int i = 0; i < 9; i++) {
+		wprintf(L"|  %lc  ", square[i]);
+		if (i == 8) {
+			printf("| \n|      |      |      |\n --------------------\n");
+			break;
+		}
+		if ((i + 1) % 3 == 0) {
+			wprintf(L"|\n|      |      |      |\n -------------------- \n|      |      |      |\n");
+		}
+
+	}
+}
+
+int CheckTurn(int turn) {
+	if (turn % 2 == 0) {
+		return CIRCLE;
+	}
+	else {
+		return CROSS;
+	}
+}
+
+int DisplayWin(CIRCLE_WIN || CROSS_WIN) {
+	if (CIRCLE_WIN) {
+		printf("〇の勝利");
+	}
+	else {
+		printf("×の勝利")
+	}
+	return EXTEND;
+}
+
+int ChangeState(int input, CIRCLE || CROSS) {
+	if (CIRCLE) {
+		square[input] = '〇';
+	}
+	else {
+		square[input] = '×';
+	}
+	return square[input];
+}
