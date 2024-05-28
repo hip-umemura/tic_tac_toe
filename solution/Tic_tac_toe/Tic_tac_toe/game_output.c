c9ab46584123d67378c6c6ddf82c6306aa95fca7
h_sato <h_sato@tr.hip-pro.net>
2024-05-28 15:56:29 +0900

    詳細仕様をもとに修正

--- solution/Tic_tac_toe/Tic_tac_toe/game_output.c
+++ solution/Tic_tac_toe/Tic_tac_toe/game_output.c
@@ -40,14 +40,13 @@ void DisplayResult(char winner_name[], BOOL judge)
 void UpdateBoard(char* square, char mark)
 {
 	*square = mark;
-	//game_info.game_board[board_index.y][board_index.x] = game_info.marks[game_info.player_index];
 }
 
 COORDINATES ToIndexOfBoard(int input_number)
 {
 	COORDINATES index;
-	index.y = input_number % BOARD_WIDTH;
-	index.x = input_number / BOARD_WIDTH;
+	index.y = input_number / BOARD_HEIGHT;
+	index.x = input_number % BOARD_WIDTH;
 
 	return index;
 }
